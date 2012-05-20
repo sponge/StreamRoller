35548842b09537708c601dfce7bc628cc1f5e7f2
Justin Fitzsimmons <justin@fitzsimmons.ca>
2012-05-20 18:54:29 -0400

    Don't need to estimate the length of a track anymore

--- src/request_handlers/flac_to_mp3.rb
+++ src/request_handlers/flac_to_mp3.rb
@@ -9,7 +9,6 @@ module StreamRoller
 
       require_tool "flac"
       require_tool "lame"
-      require_tool "shntool"
 
       config_name "flac_to_mp3"
 
@@ -31,21 +30,6 @@ module StreamRoller
 
       def transcode_flac_to_mp3
         @toolman.pipe("flac", "-s -d -c \"#{@filepath}\"").pipe("lame", "--silent --preset standard - -").io
-      end
-
-      def estimate_transcoded_mp3_size
-        shntool = @toolman.invoke("shntool", "info \"#{@filepath}\"")
-        shnput = shntool.read()
-        data = Utils::shntool_parse(shnput)
-        
-        length = data["Length"]
-        p = length.partition(":")
-        minutes = p[0].to_i
-        seconds = p[2].partition(".")[0].to_i
-        ms = p[2].partition(".")[2].to_i
-        total = ms + seconds * 1000 + minutes * 60 * 1000
-        size = total * @config['bitrate'].to_i
-        size = (size.to_f / 8.0).ceil
       end
     end
   end
